9b5b6e3657f712d20f97b4d31d605de85d1c9390
Merwan00 <135138625+Merwan00@users.noreply.github.com>
2023-09-26 12:18:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from tab_rapportseismes import rapports_seismes
 st.set_page_config(
     page_title="Carte de Sismicité",
     page_icon=":earthquake:",
-    layout="wide",  # Utiliser une mise en page plus large
+    layout="wide",  # Utiliser une mise en page plus large !
     initial_sidebar_state="expanded",  # Barre latérale ouverte par défaut
 )
 
